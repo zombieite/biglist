7d147294e57711606d35523ab6e8df9680b2190e
zombieite <387685+zombieite@users.noreply.github.com>
2026-06-23 22:09:00 +1000

    txt

--- code/biglist.pl
+++ code/biglist.pl
@@ -2385,7 +2385,7 @@ qq|Take exit 320, head east a bit, then take the dirt road north half a mile. St
             blurb   => qq||,
         },
         {
-            name    => "Mojave National Preserve South Entrance",
+            name    => "Mojave National Preserve Kelbaker Rd South Entrance",
             address => "Needles Freeway and Kelbaker Rd, CA",
             blurb   => qq|This destination is mainly included to keep you off of some closed sections of Route 66.|,
         },
