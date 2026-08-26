4de06e32f0b37e6704e38df7f5c433611bfe8239
zombieite <387685+zombieite@users.noreply.github.com>
2026-08-27 07:56:06 +1000

    txt

--- code/biglist.pl
+++ code/biglist.pl
@@ -1533,7 +1533,7 @@ qq|The current owners took over this place in 1980 and I don't think their price
         },
         {
             name    => "Seaba Station",
-            address => "336992 E OK-66, Warwick OK",
+            address => "336992 OK-66, Warwick OK",
             blurb   => qq||,
         },
         {
